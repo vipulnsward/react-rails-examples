60cb94ddf22b6eb8b7020db21afb46fb38aa5492
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-02-05 18:48:26 +0530

    WIP

---
+++ client/app/bundles/Posts/startup/HelloWorldAppClient.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Posts from '../containers/Posts';
+
+export default (props) => {
+  return (
+    <Posts {...props} />
+  );
+};
---
+++ client/app/bundles/Posts/startup/clientRegistration.jsx
@@ -0,0 +1,5 @@
+import ReactOnRails from 'react-on-rails';
+import PostsApp from './PostsAppClient';
+
+// This is how react_on_rails can see the PostsApp in the browser.
+ReactOnRails.register({ PostsApp });
